c7c1ce07056083c31dacbf697be0e3c6d494c773
John <johnlouisstephenson@gmail.com>
2024-05-31 11:44:54 +0100

    Created product model

---
+++ furniture-store-laravel/database/migrations/2024_05_31_102927_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->integer('width');
+            $table->integer('height');
+            $table->integer('depth');
+            $table->decimal('price');
+            $table->integer('stock');
+            $table->integer('related');
+            $table->string('color');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
